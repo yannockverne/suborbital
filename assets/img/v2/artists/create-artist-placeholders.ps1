912d9b793ceb88e3fc33ca530ea1a298b11af5d7
Yannock_ <yannock.f@gmail.com>
2026-08-20 10:45:50 +0200

    Pull artist webp images placeholders

---
+++ assets/img/v2/artists/create-artist-placeholders.ps1
@@ -0,0 +1,15 @@
+﻿$artists = @(
+    "lyra",
+    "kovah",
+    "madi",
+    "darren",
+    "scrapliners",
+    "sublight-shift",
+    "nok"
+)
+
+foreach ($artist in $artists) {
+    Copy-Item ".\placeholder-hero.webp"     ".\$artist-hero.webp"
+    Copy-Item ".\placeholder-portrait.webp" ".\$artist-portrait.webp"
+    Copy-Item ".\placeholder-ending.webp"   ".\$artist-ending.webp"
+}
